dec257501d44f083e8fb7dcd70a0e8163810c9de
Tom Tran <tom.tran@barreldev.com>
2021-07-11 03:21:20 +0700

    Update action header level 2

--- modules/header/header.js
+++ modules/header/header.js
@@ -1,23 +1,57 @@
 var $ = require('jquery');
-var stickyfill = require('stickyfill');
-var throttle = require('lodash.throttle');
-
-// variables
-var breakpoint = 999
-var body = document.body
-var lastScrollTop = 0
 
 function Header(el) {
   var $el = $(el);
-  var menu = $('.js-header-menu')
-  console.log('-----menu')
-  console.log(menu)
-  console.log($el)
+  var menu = $('.js-header-menu');
+  var nav = $('.js-nav');
+  var navClose = $('.js-nav-close');
+  var headerMobile = $('.js-btn-mobile');
+  var body = $('body');
 
-  var stickyPolyfill = stickyfill()
-  if (menu) {
-    stickyPolyfill.add(menu)
+  function triggerNavbar() {
+    headerMobile.toggleClass('is-active');
+    nav.toggleClass('is-active');
+    body.toggleClass('is-header-nav');
+
+    if (nav.hasClass('is-active')) {
+      nav.toggleClass('is-animated');
+    } else {
+      setTimeout(function() {
+        nav.removeClass('is-animated')
+      }, 300);
+    }
   }
+
+  headerMobile.on('click', triggerNavbar);
+  navClose.on('click', triggerNavbar);
+
+  // Parent menu click
+  var parentMenu = $('.js-link');
+
+  function toggleSubNavByClick() {
+    var link = $(this);
+    var isActive = link.hasClass('is-active');
+    var group = link.find('.js-subnav');
+    var groupInner = link.find('.header__subnav-inner');
+    var groupHeight = groupInner.height();
+
+    // close all group
+    if (group.length > 0) {
+      if (!isActive) {
+        group.height(groupHeight);
+        link.addClass('is-active');
+        group.addClass('is-active');
+      } else {
+        group.height(0);
+        link.removeClass('is-active');
+        group.removeClass('is-active');
+      }
+    }
+  }
+
+  parentMenu.on('click', toggleSubNavByClick);
+  // parentMenu.on('mouseenter', openSubNavOnEnter);
+  // parentMenu.on('mouseleave', closeSubNavOnLeave);
 }
 
 module.exports = Header;
